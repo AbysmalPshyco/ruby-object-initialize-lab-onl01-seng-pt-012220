e9ebd0e985ce6fdcb1b402775180090170620e49
Anthony Risch <rischthefisch@gmail.com>
2020-02-07 18:44:36 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -6,11 +6,11 @@ end
 attr_accessor :name 
 
 attr_accessor :breed 
- if @breed == "Pug"
-  puts "Pug"
- else
-   puts "Mutt"
-  end
+# if @breed == "Pug"
+#   puts "Pug"
+# else
+#   puts "Mutt"
+#   end
 end
 
 fido = Dog.new("Fido","Pug")
